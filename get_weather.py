bdff29f13ac1de0a7d3f6f03c00a15ab2a50922b
rneureuther1 <rneureuther1@users.noreply.github.com>
2015-08-07 10:27:34 -0400

    Create get_weather.py

---
+++ get_weather.py
@@ -0,0 +1,14 @@
+Enter file contents here
+#!/usr/bin/python
+import Adafruit_BBIO.GPIO as GPIO
+import time
+import json
+
+
+GPIO.setup()
+GPIO.setup()
+GPIO.setup()
+
+webpage = urlopen(//INSERT LINK HERE)
+print webpage
+
