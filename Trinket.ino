2b791542efe03a94202b73c0b3db61b91ffae1bd
Reid Neureuther <rneureuther1@users.noreply.github.com>
2015-08-20 10:02:06 -0400

    Update Trinket.ino

--- Trinket.ino
+++ Trinket.ino
@@ -1,46 +1,66 @@
 // Adafruit Trinket code
 // Bealgebone Tx pin to Trinket Rx pin
-// Adruino shares the same buffer for computer Serial and RxTx pins
+// Adruino shares the same buffer for computer Serial and RxTx pins but Trinket does not support USB Serial
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 6
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(24, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() 
-{
-    
-//Serial setup
-Serial.begin(9600);
-pinMode(13, OUTPUT);
-digitalWrite(13, LOW);
+{   //Serial setup
+    Serial.begin(9600);
+    pinMode(13, OUTPUT);
+    digitalWrite(13, LOW);
+
+    strip.begin();
+    strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() 
 {
+    int test;
+    test=Serial.read();
 
-int test2;
-test2=Serial.read();
+    // -1 connected, no data
+    // 0 not connected
+    // ## incoming data
 
-// -1 connected, no data
-// 0 not connected
-// ## incoming data
+    if(test==-1)
+    {
+        delay(5000);
+    }
 
-if(test2==-1)
+    else if(test==0)
+    {
+
+    }
+
+    else{
+        // If data is recieved blink twice
+        blink(13, 2);
+    }
+
+    }
+
+
+
+void blink(int pin, int times)
 {
-    delay(5000);
-}
-
-else if(test2==0)
-{
-
-}
-
-else{
-    // If data is recieved blink twice
-digitalWrite(13, HIGH);
-delay(500);
-digitalWrite(13, LOW);
-delay(500);
-digitalWrite(13, HIGH);
-delay(500);
-digitalWrite(13, LOW);
-    delay(3000);
-}
-
+    int i;
+    for(i=0; i<times; i++)
+    {
+        digitalWrite(pin, LOW);
+        delay(500);
+        digitalWrite(pin, HIGH);
+        delay(500);
+        digitalWrite(pin, LOW);
+        delay(500);
+    }
+    
 }
